68b58ba13c060ea31d7c19163ddf247f558e67ec
vincellob <vincellob@yahoo.com>
2024-12-11 09:32:59 -0500

    Initital commit

---
+++ ticketing-reimbursement-system/src/main/java/com/revature/training/ticketing_reimbursement_system/TicketingReimbursementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.revature.training.ticketing_reimbursement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingReimbursementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingReimbursementSystemApplication.class, args);
+	}
+
+}
---
+++ ticketing-reimbursement-system/src/test/java/com/revature/training/ticketing_reimbursement_system/TicketingReimbursementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.training.ticketing_reimbursement_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingReimbursementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
